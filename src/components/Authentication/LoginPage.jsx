6f5d88b60abc08857e8831b41c9912318c4f8271
Abishek Sam <samabishek75@gmail.com>
2025-05-13 21:33:52 +0530

    Second Commit

---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import "./LoginPage.css";
+
+const schema = z.object({
+  email: z.string().email().min(3),
+  password: z.string().min(8),
+});
+
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange", resolver: zodResolver(schema) });
+
+  const onSubmit = (formData) => console.log(formData);
+
+  return (
+    <section className="align_center form_page">
+      <form className="authentication_form" onSubmit={onSubmit}>
+        <h2>Login Form</h2>
+        <div className="form_inputs">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="form_text_input"
+              placeholder="Enter your email address"
+              {...register("email")}
+            />
+            {errors.email && (
+              <em className="form_error">{errors.email.message}</em>
+            )}
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="form_text_input"
+              placeholder="Enter your password"
+              {...register("password")}
+            />
+            {errors.password && (
+              <em className="form_error">{errors.password.message}</em>
+            )}
+          </div>
+
+          <button className="search_button form_submit">Submit</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/Products/ProductsList.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./ProductsList.css";
+import ProductCard from "./ProductCard";
+import useData from "../../hooks/useData";
+import ProductCardSkeleton from "./ProductCardSkeleton";
+import { useSearchParams } from "react-router-dom";
+import Pagination from "../Common/Pagination";
+
+const ProductsList = () => {
+  const [search, setSerach] = useSearchParams();
+  const category = search.get("category");
+  const page = search.get("page");
+
+  const { data, error, isLoading } = useData(
+    "/products",
+    {
+      params: { category, page },
+    },
+    [category, page]
+  );
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const handlePageChange = (page) => {
+    const currentParams = Object.fromEntries([...search]);
+    setSerach({ ...currentParams, page: page });
+  };
+
+  return (
+    <section className="products_list_section">
+      <header className="align_center products_list_header">
+        <h2>Products</h2>
+        <select name="sort" id="" className="products_sorting">
+          <option value="">Relevance</option>
+          <option value="price desc">Price HIGH to LOW</option>
+          <option value="price asc">Price LOW to HIGH</option>
+          <option value="rate desc">Rate HIGH to LOW</option>
+          <option value="rate asc">Rate LOW to HIGH</option>
+        </select>
+      </header>
+      <div className="products_list">
+        {error && <em className="form_error">{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
+        {data?.products &&
+          data.products.map((product) => (
+            <ProductCard
+              key={product._id}
+              id={product._id}
+              image={product.images[0]}
+              price={product.price}
+              title={product.title}
+              rating={product.reviews.rate}
+              ratingCounts={product.reviews.counts}
+              stock={product.stock}
+            />
+          ))}
+      </div>
+      <Pagination totalPosts={24} postsPerPage={8} onClick={handlePageChange} />
+    </section>
+  );
+};
+
+export default ProductsList;
